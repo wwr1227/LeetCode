dd9e4406e76cdf4223ac07d4131841ede26d6673
wwr1227 <50442046@qq.com>
2013-08-09 21:26:06 +0800

    Create SolutionUpdated.java
    No ide
    1A

---
+++ src/TwoSum/SolutionUpdated.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+      
+		int[] ans ={0,0};
+        if(numbers==null || numbers.length==0)
+			return ans;
+		
+		int len = numbers.length;
+		Map<Integer, Integer> hash = new HashMap<Integer,Integer>();
+		for(int i=0;i<len ;i++){
+			hash.put(numbers[i],i);
+		}
+		for(int i=0;i<len ;i++){
+			if(hash.containsKey(target-numbers[i])){
+				ans[0]=i+1;
+				ans[1]=hash.get(target-numbers[i])+1;
+				break;
+			}
+		}
+		return ans;
+    }
+}
