ea066af277552324691bcb6b63e36d596e34f758
wwr1227 <50442046@qq.com>
2013-08-10 14:38:23 +0800

    Create SolutionUpdated.java

---
+++ src/ContainerWithMostWater/SolutionUpdated.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+      if(height==null || height.length==0)
+			return 0;
+		
+		int ans = 0;
+		int i = 0;
+		int j=height.length-1;
+		while(i<j){
+			ans = Math.max(ans, Math.min(height[i],height[j])*(j-i));
+			if(height[i]<height[j])
+				i++;
+			else
+				j--;
+		}
+		return ans;
+    }
+} 
