fbe0597f0a6944cbf948b3bc7ff78323c33653e9
wwr1227 <50442046@qq.com>
2013-08-10 17:00:09 +0800

    Update SolutionUpdated.java

--- src/Remove-Nth-Node-From-End-of-List/SolutionUpdated.java
+++ src/Remove-Nth-Node-From-End-of-List/SolutionUpdated.java
@@ -13,21 +13,21 @@ public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
-      if(n<=0)
-			return head;
-		ListNode virtul = new ListNode(0);
-		virtul.next=head;
-		ListNode fastNode = virtul;
-		ListNode slowNode = virtul;
-		int index=0;
-		while(fastNode.next!=null){
-			fastNode=fastNode.next;
-			index++;
-			if(index>n){
-				slowNode=slowNode.next;
-			}
+      	if(n<=0)
+		return head;
+	ListNode virtul = new ListNode(0);
+	virtul.next=head;
+	ListNode fastNode = virtul;
+	ListNode slowNode = virtul;
+	int index=0;
+	while(fastNode.next!=null){
+		fastNode=fastNode.next;
+		index++;
+		if(index>n){
+			slowNode=slowNode.next;
 		}
+	}
         slowNode.next =  slowNode.next.next;
-		return virtul.next;	
+	return virtul.next;	
     }
 }
