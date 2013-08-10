d2c08363b2435ef081cc1caf4f3d90937078486d
wwr1227 <50442046@qq.com>
2013-08-10 10:33:47 +0800

    Create SolutionUpdated.java

---
+++ src/PalindromeNumber/SolutionUpdated.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+  	// Start typing your Java solution below
+		// DO NOT write main() function
+        if (x < 0)
+    		return false;
+		int divisor = 1;
+		int xDummy = x;
+		while (x / 10 != 0) {
+			divisor *= 10;
+			x /= 10;
+
+		}
+		while (xDummy != 0 && divisor != 0) {
+			if (xDummy < 10 && xDummy > -10)
+				return true;
+			if ((xDummy / divisor) % 10 != xDummy % 10)
+				return false;
+			divisor /= 100;
+			xDummy /= 10;
+		}
+		return true;
+	}
+}
