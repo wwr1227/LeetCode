94fc950a85aee58437273b00f267682eb1f1405e
wwr1227 <50442046@qq.com>
2013-08-11 11:17:43 +0800

    Create SolutionUpdated.java

---
+++ src/RemoveElement/SolutionUpdated.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(A==null || A.length<1)
+      	return 0;
+		int index=-1;
+		for(int i=0;i<A.length;i++){
+			if(A[i]!=elem)
+				A[++index]=A[i];
+		}
+		return index+1;
+    }
+}
