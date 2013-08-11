507a015001447576162401a7fdb0a780d42f3195
wwr1227 <50442046@qq.com>
2013-08-11 10:31:54 +0800

    Create SolutionUpdated.java

---
+++ src/Swap-Nodes-in-Pairs/SolutionUpdated.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+      
+		if (head==null)
+			return head;
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		while(head!=null && head.next!=null){
+			int temp = head.val;
+			head.val = head.next.val;
+			head.next.val = temp;
+			head = head.next.next;
+		}
+        return dummy.next;
+    }
+}
