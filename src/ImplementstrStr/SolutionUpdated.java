215c393d3805359559c78686605823b4fc83a4a8
wwr1227 <50442046@qq.com>
2013-08-11 11:31:41 +0800

    Update SolutionUpdated.java

--- src/ImplementstrStr/SolutionUpdated.java
+++ src/ImplementstrStr/SolutionUpdated.java
@@ -7,16 +7,16 @@ public class Solution {
         if(needle.length()==0)
             return haystack; 
   	
-		for(int i=0;i<haystack.length()-needle.length()+1;i++){
-			int needleIndex=0;
-			int haystackIndex=0;
-			while(haystack.charAt(i+haystackIndex)==needle.charAt(needleIndex)){
-				needleIndex++;
-				haystackIndex++;
-				if(needleIndex==needle.length())
-					return haystack.substring(i);
-			}
+	for(int i=0;i<haystack.length()-needle.length()+1;i++){
+		int needleIndex=0;
+		int haystackIndex=0;
+		while(haystack.charAt(i+haystackIndex)==needle.charAt(needleIndex)){
+			needleIndex++;
+			haystackIndex++;
+			if(needleIndex==needle.length())
+				return haystack.substring(i);
 		}
-		return null;
+	}
+	return null;
     }
 }
