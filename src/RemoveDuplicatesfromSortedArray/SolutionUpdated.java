41be24e269045c6b794043d892d63b4eac9e73df
wwr1227 <50442046@qq.com>
2013-08-11 11:13:40 +0800

    Update SolutionUpdated.java

--- src/RemoveDuplicatesfromSortedArray/SolutionUpdated.java
+++ src/RemoveDuplicatesfromSortedArray/SolutionUpdated.java
@@ -3,12 +3,12 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         if(A==null || A.length==0)
-      	return 0;
-		int index=0;
-		for(int i=1;i<A.length;i++){
-			if(A[i]!=A[i-1])
-				A[++index] = A[i];
-		}
-		return index+1;
+      		return 0;
+	int index=0;
+	for(int i=1;i<A.length;i++){
+		if(A[i]!=A[i-1])
+			A[++index] = A[i];
+	}
+	return index+1;
     }
 }
