c78d67982b610863e22e0b9e848c4aae24732d85
wwr1227 <50442046@qq.com>
2013-08-11 11:13:20 +0800

    Create SolutionUpdated.java

---
+++ src/RemoveDuplicatesfromSortedArray/SolutionUpdated.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(A==null || A.length==0)
+      	return 0;
+		int index=0;
+		for(int i=1;i<A.length;i++){
+			if(A[i]!=A[i-1])
+				A[++index] = A[i];
+		}
+		return index+1;
+    }
+}
