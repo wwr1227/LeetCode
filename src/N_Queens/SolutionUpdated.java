48d8b6fec38c54abcde6d2a732a77a75dca51509
wwr1227 <50442046@qq.com>
2013-08-19 07:46:26 +0800

    Update SolutionUpdated.java

--- src/N_Queens/SolutionUpdated.java
+++ src/N_Queens/SolutionUpdated.java
@@ -1,24 +1,35 @@
 public class Solution {
-    int totalQueens = 0;
-    public int totalNQueens(int n) {
+    public ArrayList<String[]> solveNQueens(int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        ArrayList<String[]> ans = new  ArrayList<String[]>();
         int[] queens= new int[n];
-        totalQueens=0;
         if(n<=0)
-            return totalQueens;
-        DFS(0,n,queens);
-        return totalQueens;
+            return ans;
+        DFS(0,n,ans,queens);
+        return ans;
     }
-    public void DFS(int cur, int n, int[] queens){
+    public void DFS(int cur, int n, ArrayList<String[]> ans , int[] queens){
         if(cur==n){
-            totalQueens++;
+            char[][] rows = new char[n][n];
+            for(int j=0;j<n;j++){
+                for(int i=0;i<n;i++)
+                    if(i==queens[j])
+                        rows[j][i]='Q';
+                    else
+                        rows[j][i]='.';
+            }
+            String[] ansStr=new String[n];
+            for(int i=0;i<n;i++){
+                ansStr[i]= String.valueOf(rows[i]);
+            }
+            ans.add(ansStr);
             return;
         }
         for(int i=0;i<n;i++){
             queens[cur] = i;
             if(canQueen(queens,cur))
-                DFS(cur+1,n,queens);
+                DFS(cur+1,n,ans,queens);
         }
     }
     public boolean canQueen(int[]queens, int pos){
