6cd2b940a308718f2f396b85fafb8b6eecdeaf46
wwr1227 <50442046@qq.com>
2013-08-18 21:31:33 +0800

    Create SolutionUpdated.java

---
+++ src/Pow/SolutionUpdated.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public double pow(double x, int n) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if (x == 0 && n == 0)
+			return 1;
+		if (x == 0)
+			return 0;
+		if (n < 0)
+			x = 1 / x;
+		return getPow(x, n);
+	}
+
+	public double getPow(double x, int n) {
+		if (n == 0)
+			return 1;
+		if (n == 1 || n == -1)
+			return x;
+		double tempPow = getPow(x, n / 2);
+		if (n % 2 == 0)
+			return tempPow * tempPow;
+		else
+			return tempPow * tempPow * x;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Solution().getPow(2, -2));
+	}
+}
