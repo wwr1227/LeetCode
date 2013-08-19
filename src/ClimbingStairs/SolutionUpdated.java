c405a66cbca6e123e1052473e58f5e869348605c
wwr1227 <50442046@qq.com>
2013-08-19 14:52:42 +0800

    Create SolutionUpdated.java

---
+++ src/ClimbingStairs/SolutionUpdated.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int climbStairs(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(n<=1)
+            return n;
+        int one = 1;
+        int two = 1;
+        int ans = 0;
+        for(int i=2;i<=n;i++){
+            ans=one+two;
+            one=two;
+            two=ans;
+        }
+        return ans;
+    }
+}
