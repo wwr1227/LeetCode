e4d905cbb4fa84beffc17e43e9b1eca0938789fa
wwr1227 <50442046@qq.com>
2013-08-21 22:30:40 +0800

    Create SolutionUpdated.java

---
+++ src/SymmetricTree/SolutionUpdated.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(root==null)
+            return true;        
+        return DFS(root.left,root.right);
+    }
+    public boolean DFS(TreeNode left, TreeNode right){
+        if(left==null&&right==null)
+            return true;
+        if(left==null||right==null)
+            return false;
+        if(left.val!=right.val)
+            return false;
+        return DFS(left.right, right.left)&& DFS(left.left, right.right);
+    }
+}
