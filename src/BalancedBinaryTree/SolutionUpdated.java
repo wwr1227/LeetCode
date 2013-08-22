227096bd804d3d11e5c0535199ad4d361d4acd4e
wwr1227 <50442046@qq.com>
2013-08-22 21:05:27 +0800

    Update SolutionUpdated.java

--- src/BalancedBinaryTree/SolutionUpdated.java
+++ src/BalancedBinaryTree/SolutionUpdated.java
@@ -8,25 +8,25 @@
  * }
  */
 public class Solution {
-    public boolean isBalanced(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-    	if(root==null)
+	public boolean isBalanced(TreeNode root) {
+	// Start typing your Java solution below
+	// DO NOT write main() function
+		if(root==null)
 			return true;
-		return !(isBalancedHandler(root)==-1);
-    }
+	    	return !(isBalancedHandler(root)==-1);
+	}
 	public int isBalancedHandler(TreeNode root){
 		if(root==null){
 			return 0;
 		}
 		int heightLeft= isBalancedHandler(root.left);
-        if(heightLeft==-1)
-            return -1;
-		int heightRight= isBalancedHandler(root.right);
-        if(heightRight==-1)
-            return -1;
+	        if(heightLeft==-1)
+	            return -1;
+			int heightRight= isBalancedHandler(root.right);
+	        if(heightRight==-1)
+	            return -1;
 		if(Math.abs(heightLeft - heightRight) <= 1) {		
-            return 1 + Math.max(heightRight, heightLeft);
+	            return 1 + Math.max(heightRight, heightLeft);
 		}else{
 			return -1;
 		}	
