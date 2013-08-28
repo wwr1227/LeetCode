fd59ddab75f9fc220f0f98729a35a5a69d321cbe
wwr1227 <50442046@qq.com>
2013-08-28 10:36:55 +0800

    Create SolutionUpdated.java

---
+++ src/FlattenBinaryTreetoLinkedList/SolutionUpdated.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(root==null)
+    		return;
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		TreeNode flattenNode = null;
+		while(!stack.isEmpty()){
+			TreeNode node = stack.pop();
+			if(node.right!=null){
+				stack.push(node.right);
+				node.right=null;
+			}
+			if(node.left!=null){
+				stack.push(node.left);
+				node.left=null;
+			}
+			if(flattenNode==null){
+				flattenNode=node;
+			}else{
+				flattenNode.left=null;
+				flattenNode.right=node;
+				flattenNode=flattenNode.right;
+			}
+		}
+    }
+}
