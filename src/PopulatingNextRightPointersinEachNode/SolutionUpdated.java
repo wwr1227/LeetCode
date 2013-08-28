a0c4601c7c4d81cd3b44df7ff525b3dc5e0d203e
wwr1227 <50442046@qq.com>
2013-08-28 14:54:35 +0800

    Update SolutionUpdated.java

--- src/PopulatingNextRightPointersinEachNode/SolutionUpdated.java
+++ src/PopulatingNextRightPointersinEachNode/SolutionUpdated.java
@@ -7,10 +7,10 @@
  * }
  */
 public class Solution {
-    public void connect(TreeLinkNode root) {
+	 public void connect(TreeLinkNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(root==null|| root.left == null || root.right == null) 
+        	if(root==null|| root.left == null || root.right == null) 
 			return;
 		root.left.next=root.right;
 		if(root.next!=null){
