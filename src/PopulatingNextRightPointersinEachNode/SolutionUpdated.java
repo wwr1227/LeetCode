ade40c050f05069138350840ec252d1a1edd73c8
wwr1227 <50442046@qq.com>
2013-08-28 14:54:20 +0800

    Create SolutionUpdated.java

---
+++ src/PopulatingNextRightPointersinEachNode/SolutionUpdated.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(root==null|| root.left == null || root.right == null) 
+			return;
+		root.left.next=root.right;
+		if(root.next!=null){
+			root.right.next = root.next.left;
+		}else{
+			root.right.next = null;
+		}
+		connect(root.left);
+		connect(root.right);
+    	
+    }
+}
