e3ac236a2a44bcb27f1f1f1ccac42e53059a4e47
wwr1227 <50442046@qq.com>
2013-08-30 16:45:05 +0800

    Create SolutionUpdated.java

---
+++ src/UniqueBinarySearchTrees/SolutionUpdated.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int numTrees(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+    	if(n<=1)
+			return 1;
+		
+		int [] dp = new int[n+1];
+		dp[0]=dp[1]=1;
+		for(int i=2;i<=n;i++)
+			for(int j=0;j<i;j++)
+				dp[i]+=dp[j]*dp[i-j-1];
+		
+        return dp[n];
+    }
+}
