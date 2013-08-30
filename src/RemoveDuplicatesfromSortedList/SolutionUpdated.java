c1a75918738c586be0b58334b26d2ba11cf34606
wwr1227 <50442046@qq.com>
2013-08-30 13:34:11 +0800

    Create SolutionUpdated.java

---
+++ src/RemoveDuplicatesfromSortedList/SolutionUpdated.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head==null||head.next==null)
+          return head;
+        ListNode cur = new ListNode(0);
+        cur.next = head;
+        ListNode ans = cur;
+        while(cur!=null){
+          ListNode pre = cur.next;
+          while(pre!=null && pre.val==cur.val)
+          	pre=pre.next;
+          cur.next = pre;
+          cur = cur.next;			
+        }
+        return ans.next;
+    }
+}
