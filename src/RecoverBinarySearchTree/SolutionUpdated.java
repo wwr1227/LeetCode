7af1ab2aa2af635700be21014a172693d40db5f9
wwr1227 <50442046@qq.com>
2013-08-30 13:16:19 +0800

    Update SolutionUpdated.java

--- src/RecoverBinarySearchTree/SolutionUpdated.java
+++ src/RecoverBinarySearchTree/SolutionUpdated.java
@@ -11,48 +11,48 @@ public class Solution {
     public void recoverTree(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-		if(root == null)
-			return;
-		
-		TreeNode parent = null;
+	if(root == null)
+		return;
+	
+	TreeNode parent = null;
         TreeNode current = root;
-		TreeNode prev = null;
+	TreeNode prev = null;
         TreeNode mistake_node1 = null;
         TreeNode mistake_node2 = null;
 		
-		while(!current){
-			if (current.left==null) {
-				if(parent!=null && parent.val > current.val){
-					if(mistake_node1==null)
-						mistake_node1=parent;
-					mistake_node2 = current;
-				}				
-				parent = current;
-				current = current.right;
-			}else{
-				//http://en.wikipedia.org/wiki/Threaded_binary_tree
-				prev = current.left;
-                while (prev.right!=null && prev.right != current)
-                    prev = prev.right;
-                if (prev.right == null) {
-                    prev.right = current;
-                    current = current.left;
-                } else {
-                    if (parent.val > current.val) {
-                        if (mistake_node1 == null) 
-                            mistake_node1 = parent;
-                        mistake_node2 = current;
-                    }
-                    prev.right = null;
-                    parent = current;
-                    current = current.right;
-                }
-            }
+	while(!current){
+		if (current.left==null) {
+			if(parent!=null && parent.val > current.val){
+				if(mistake_node1==null)
+					mistake_node1=parent;
+				mistake_node2 = current;
+			}				
+			parent = current;
+			current = current.right;
+		}else{
+			//http://en.wikipedia.org/wiki/Threaded_binary_tree
+			prev = current.left;
+		        while (prev.right!=null && prev.right != current)
+		            prev = prev.right;
+		        if (prev.right == null) {
+		            prev.right = current;
+		            current = current.left;
+		        } else {
+		            if (parent.val > current.val) {
+		                if (mistake_node1 == null) 
+		                    mistake_node1 = parent;
+		                mistake_node2 = current;
+		            }
+		            prev.right = null;
+		            parent = current;
+		            current = current.right;
+		        }
+		}
         }
         if (mistake_node1!=null && mistake_node2!=null){
-			int temp =mistake_node1.val;
-			mistake_node1.val = mistake_node2.val;
-			mistake_node2.val = temp;
-		}
+		int temp =mistake_node1.val;
+		mistake_node1.val = mistake_node2.val;
+		mistake_node2.val = temp;
+	}
     }
 }
