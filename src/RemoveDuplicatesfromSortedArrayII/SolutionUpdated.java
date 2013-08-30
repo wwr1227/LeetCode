f547e6ee0a5262ef9784a9a52a4af8297cdf543d
wwr1227 <50442046@qq.com>
2013-08-30 17:18:52 +0800

    Update SolutionUpdated.java

--- src/RemoveDuplicatesfromSortedArrayII/SolutionUpdated.java
+++ src/RemoveDuplicatesfromSortedArrayII/SolutionUpdated.java
@@ -3,22 +3,22 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
     	if(A.length<=2)
-			return A.length;
-		
-		int len = A.length;
-		int count = 1;
-		int post = 0;
-		for(int i=1;i<len;i++){
-			if(A[post] == A[i]){
-				if(count<2){
-					count++;
-					A[++post] = A[i];
-				}
-			}else{
-				count=1;
+		return A.length;
+	
+	int len = A.length;
+	int count = 1;
+	int post = 0;
+	for(int i=1;i<len;i++){
+		if(A[post] == A[i]){
+			if(count<2){
+				count++;
 				A[++post] = A[i];
 			}
+		}else{
+			count=1;
+			A[++post] = A[i];
 		}
+	}
         return post+1;
     }
 }
