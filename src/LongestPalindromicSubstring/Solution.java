e6a45e8511a437a27a14de018806eb69782a1795
wwr1227 <50442046@qq.com>
2013-09-01 00:45:08 +0800

    Create Solution.java

---
+++ src/LongestPalindromicSubstring/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
