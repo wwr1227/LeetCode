7541507c5e42fe5ef0b196352e63b226bdf8794d
wwr1227 <50442046@qq.com>
2013-08-31 19:12:32 +0800

    Create SolutionUpdated.java

---
+++ src/BestTimetoBuyandSellStock/SolutionUpdated.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+    	if(prices==null||prices.length==0)
+			return 0;
+		int minPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+		for(int i=0;i<prices.length;i++){
+			minPrice=Math.min(minPrice,prices[i]);
+			maxProfit=Math.max(maxProfit,prices[i]-minPrice);
+		}
+        return maxProfit;
+    }
+}
