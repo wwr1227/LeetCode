c3916bb2461d7bbbea001ee9a96296dab5a5a471
wwr1227 <50442046@qq.com>
2013-08-31 18:53:26 +0800

    Update SolutionUpdated.java

--- src/PathSumII/SolutionUpdated.java
+++ src/PathSumII/SolutionUpdated.java
@@ -8,16 +8,16 @@
  * }
  */
 public class Solution {
-    public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-    	ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+	public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
+	        // Start typing your Java solution below
+	        // DO NOT write main() function
+	    	ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> item = new ArrayList<Integer>();
 		if(root==null)
 			return ans;
 		dfs(root,sum,ans,item);
 		return ans;
-    }
+	}
 	public void dfs(TreeNode node, int sum, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> item){
 		if(node==null)
 			return;
@@ -25,7 +25,7 @@ public class Solution {
 		if(node.left==null&&node.right==null){
 			if(sum==node.val)
 				ans.add(new ArrayList<Integer>(item));
-            item.remove(item.size()-1);			
+			item.remove(item.size()-1);			
 			return;
 		}
 		dfs(node.left,sum-node.val,ans,item);
