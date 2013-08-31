e5d4cf6ab5f70a90e81416d3818aa70257c011a6
wwr1227 <50442046@qq.com>
2013-08-31 19:12:45 +0800

    Update SolutionUpdated.java

--- src/BestTimetoBuyandSellStock/SolutionUpdated.java
+++ src/BestTimetoBuyandSellStock/SolutionUpdated.java
@@ -3,13 +3,13 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
     	if(prices==null||prices.length==0)
-			return 0;
-		int minPrice = Integer.MAX_VALUE;
-		int maxProfit = 0;
-		for(int i=0;i<prices.length;i++){
-			minPrice=Math.min(minPrice,prices[i]);
-			maxProfit=Math.max(maxProfit,prices[i]-minPrice);
-		}
+		return 0;
+	int minPrice = Integer.MAX_VALUE;
+	int maxProfit = 0;
+	for(int i=0;i<prices.length;i++){
+		minPrice=Math.min(minPrice,prices[i]);
+		maxProfit=Math.max(maxProfit,prices[i]-minPrice);
+	}
         return maxProfit;
     }
 }
