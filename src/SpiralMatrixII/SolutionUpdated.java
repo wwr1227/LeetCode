bbb48a7c196beae2203615ce28ed566324b0d951
wwr1227 <50442046@qq.com>
2013-08-31 22:03:11 +0800

    Update SolutionUpdated.java

--- src/SpiralMatrixII/SolutionUpdated.java
+++ src/SpiralMatrixII/SolutionUpdated.java
@@ -3,25 +3,25 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
     	if(n<0)
-			return null;
-		int [][] ans = new int[n][n];
-		int start = 0;
-		int end = n-1;
-		int num=1;
-		while(start<end){
-			for(int i=start;i<end;i++)
-				ans[start][i]=num++;
-			for(int i=start;i<end;i++)
-				ans[i][end]=num++;
-			for(int i=end;i>start;i--)
-				ans[end][i]=num++;
-			for(int i=end;i>start;i--)
-				ans[i][start]=num++;
-			start++;
-			end--;
-		}
-		if(start==end)
-			ans[start][end]=num;
-		return ans;
+		return null;
+	int [][] ans = new int[n][n];
+	int start = 0;
+	int end = n-1;
+	int num=1;
+	while(start<end){
+		for(int i=start;i<end;i++)
+			ans[start][i]=num++;
+		for(int i=start;i<end;i++)
+			ans[i][end]=num++;
+		for(int i=end;i>start;i--)
+			ans[end][i]=num++;
+		for(int i=end;i>start;i--)
+			ans[i][start]=num++;
+		start++;
+		end--;
+	}
+	if(start==end)
+		ans[start][end]=num;
+	return ans;
     }
 }
