dd672a96667914b5855ab0881ec8fdbc660e23d4
wwr1227 <50442046@qq.com>
2013-09-01 17:04:49 +0800

    O(n)

--- src/LongestValidParentheses/SolutionUpdated.java
+++ src/LongestValidParentheses/SolutionUpdated.java
@@ -1,25 +1,26 @@
+//Time complexity:O(n)
 public class Solution {
-    public int longestValidParentheses(String s) {
+	public int longestValidParentheses(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-      if(s==null||s.length()==0)
-    		return 0;
+		if(s==null||s.length()==0)
+	    		return 0;
 		Stack<Integer> stack = new Stack<Integer>();
 		stack.push(-1);
 		int ans = 0;
 		for (int i = 0; i < s.length(); i++){
-           if (s.charAt(i) == '(')
-               stack.push(i);
-           else{
-               if (stack.size() == 1){
-                   stack.pop();
-                   stack.push(i);                    
-                   continue;
-               }
-               stack.pop();
-               ans = Math.max(ans, i - stack.peek());            
-           }
-       }
-       return ans;
-   }
+	   		if (s.charAt(i) == '(')
+	       			stack.push(i);
+	   		else{
+	       			if (stack.size() == 1){
+	           			stack.pop();
+	           			stack.push(i);                    
+	           			continue;
+	       			}
+	       			stack.pop();
+	       			ans = Math.max(ans, i - stack.peek());            
+	   		}
+		}
+       		return ans;
+   	}
 }
