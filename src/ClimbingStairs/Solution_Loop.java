bc351b20990ce30befe02e255a4a0b0732aaaed8
wwr1227 <50442046@qq.com>
2013-09-01 10:49:26 +0800

    O(n)

---
+++ src/ClimbingStairs/Solution_Loop.java
@@ -0,0 +1,17 @@
+//Time complexity:O(n)
+public class Solution {
+    public int climbStairs(int n) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int ans=0;
+        if(n<=1) return ans;
+        int f0=1;
+        int f1=1;
+        for(int i=2;i<=n;i++){
+            ans=f0+f1;
+            f0=f1;
+            f1=ans;
+        }
+        return ans;
+    }
+}
