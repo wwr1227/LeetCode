c4de61ea67864395d33491b348fc791629c06a36
wwr1227 <50442046@qq.com>
2013-09-01 21:25:12 +0800

    O(n)

--- src/Minimum-Depth-of-Binary-Tree/Solution_DFS.java
+++ src/Minimum-Depth-of-Binary-Tree/Solution_DFS.java
@@ -1,3 +1,4 @@
+//Time complexity:O(n)
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -12,14 +13,11 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         
-    if(root==null)
-			return 0;
-		if(root.left==null && root.right==null)
-			return 1;
-		if(root.left!=null && root.right==null)
-			return minDepth(root.left)+1;
-		if(root.right!=null&& root.left==null)
-			return minDepth(root.right)+1;
-		return Math.min(minDepth(root.left)+1,minDepth(root.right)+1);
+	if (root == null)  return 0;
+	int left = minDepthDFS1(root.left);
+	int right = minDepthDFS1(root.right);
+	if (right == 0)  return left+1;
+	if (left == 0)  return right+1;
+	return Math.min(left, right)+1;
     }
 }
