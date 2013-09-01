2c0822412b541a8e30282fa4b0d27199d1d6cedf
wwr1227 <50442046@qq.com>
2013-09-01 15:49:02 +0800

    O(n^2)

--- src/JumpGame/SolutionUpdated.java
+++ src/JumpGame/SolutionUpdated.java
@@ -1,20 +1,21 @@
+//Time complexity:O(n^2)
 public class Solution {
     public boolean canJump(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
         if(A==null || A.length<=1)
     		return true;
-		int maxDistance = 0;
-		int end=0;
-		int begin=0;
-		while(end<A.length-1){
-			for(int i=begin;i<=end;i++)
-				maxDistance = Math.max(maxDistance, A[i]+i);
-			if(maxDistance<=end)
-				return false;
-			begin=end+1;
-			end=maxDistance;
-		}
-		return true;
+	int maxDistance = 0;
+	int end=0;
+	int begin=0;
+	while(end<A.length-1){
+		for(int i=begin;i<=end;i++)
+			maxDistance = Math.max(maxDistance, A[i]+i);
+		if(maxDistance<=end)
+			return false;
+		begin=end+1;
+		end=maxDistance;
+	}
+	return true;
     }
 }
