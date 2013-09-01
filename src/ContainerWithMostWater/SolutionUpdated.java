b7585f41c3d7af769cd2acab28950989bcb3810d
wwr1227 <50442046@qq.com>
2013-09-01 11:38:47 +0800

    O(n)

--- src/ContainerWithMostWater/SolutionUpdated.java
+++ src/ContainerWithMostWater/SolutionUpdated.java
@@ -1,3 +1,4 @@
+//Time complexity:O(n)
 public class Solution {
     public int maxArea(int[] height) {
         // Start typing your Java solution below
@@ -5,17 +6,16 @@ public class Solution {
         
       if(height==null || height.length==0)
 			return 0;
-		
-		int ans = 0;
-		int i = 0;
-		int j=height.length-1;
-		while(i<j){
-			ans = Math.max(ans, Math.min(height[i],height[j])*(j-i));
-			if(height[i]<height[j])
-				i++;
-			else
-				j--;
-		}
-		return ans;
+	int ans = 0;
+	int i = 0;
+	int j=height.length-1;
+	while(i<j){
+		ans = Math.max(ans, Math.min(height[i],height[j])*(j-i));
+		if(height[i]<height[j])
+			i++;
+		else
+			j--;
+	}
+	return ans;
     }
 } 
