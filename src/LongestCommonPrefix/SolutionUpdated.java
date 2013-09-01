6e81a46b24065aa002706da98909278148d5486f
wwr1227 <50442046@qq.com>
2013-09-01 16:38:07 +0800

    O(k*n)

--- src/LongestCommonPrefix/SolutionUpdated.java
+++ src/LongestCommonPrefix/SolutionUpdated.java
@@ -1,20 +1,20 @@
+//Time complexity:O(k*n)
 public class Solution {
-    public String longestCommonPrefix(String[] strs) {
+    	public String longestCommonPrefix(String[] strs) {
         // Start typing your Java solution below
         // DO NOT write main() function
         
-  	if(strs==null || strs.length==0)
+		if(strs==null || strs.length==0||strs[0].length()==0)
 			return "";
 		
-	    String prefix = strs[0];
-		for(int i=1;i<strs.length;i++){
-			for(int j=0;j<prefix.length();j++){
-				if(j==strs[i].length() || prefix.charAt(j)!=strs[i].charAt(j)){
-					if(j==0) return "";
-					prefix = prefix.substring(0,j);
-				}
+        	for (int j = 0; j < strs[0].length(); j++) {
+    			for (int i = 1; i < strs.length; i++) {
+				if (j >= strs[i].length()) 
+					return strs[0].substring(0, strs[i].length());
+				if (strs[i].charAt(j) != strs[0].charAt(j))
+                    return strs[0].substring(0, j);
 			}
 		}
-        return prefix;
-    }
+		return strs[0];
+    	}
 }
