efef5794bcbb96b86619c24b5a4a133a2a1cecca
wwr1227 <50442046@qq.com>
2013-09-02 13:05:46 +0800

    O(log(m*n))

--- src/Searcha2DMatrix/Solution_BinarySearch.java
+++ src/Searcha2DMatrix/Solution_BinarySearch.java
@@ -1,9 +1,9 @@
 //O(log(m*n))
 public class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    	public boolean searchMatrix(int[][] matrix, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-    	if(matrix==null||matrix.length==0)
+    		if(matrix==null||matrix.length==0)
 			return false;
 		
 		int m=matrix.length;
@@ -21,6 +21,6 @@ public class Solution {
 			else
 				left=mid+1;
 		}
-        return false;
-    }
+        	return false;
+    	}
 }
