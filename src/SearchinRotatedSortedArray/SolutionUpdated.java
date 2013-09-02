f64787017645abd28a6d710f0d502928665ee192
wwr1227 <50442046@qq.com>
2013-09-02 23:12:24 +0800

    O(logn)

--- src/SearchinRotatedSortedArray/SolutionUpdated.java
+++ src/SearchinRotatedSortedArray/SolutionUpdated.java
@@ -1,17 +1,16 @@
+//O(logn)
 public class Solution {
-    public int search(int[] A, int target) {
+	public int search(int[] A, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        if(A==null || A.length==0)
-          return -1;
+        	if(A==null || A.length==0)
+          		return -1;
 		int left=0;
 		int right = A.length-1;
-		
 		while(left<=right){
 			int mid = left+(right-left)/2;
 			if(A[mid]==target)
 				return mid;
-				
 			if(A[left]<=A[mid]){
 				if (A[left] <= target && target < A[mid])
 					right = mid - 1;
@@ -19,11 +18,11 @@ public class Solution {
 					left = mid + 1;
 			}else{
 				if (A[mid] < target && target <= A[right])
-                    left = mid + 1;
-                else
-                    right = mid - 1;
+                    			left = mid + 1;
+                	else
+                    			right = mid - 1;
 			}
 		}
 		return -1;
-    }
+    	}
 }
