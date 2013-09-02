fa3b2a00b1c8cef88009f82de015be4f563bed89
wwr1227 <50442046@qq.com>
2013-09-02 14:31:09 +0800

    O(n)

--- src/PalindromeNumber/SolutionUpdated.java
+++ src/PalindromeNumber/SolutionUpdated.java
@@ -1,15 +1,15 @@
+//O(n)
 public class Solution {
     public boolean isPalindrome(int x) {
   	// Start typing your Java solution below
 		// DO NOT write main() function
-        if (x < 0)
-    		return false;
+        	if (x < 0)
+    			return false;
 		int divisor = 1;
 		int xDummy = x;
 		while (x / 10 != 0) {
 			divisor *= 10;
 			x /= 10;
-
 		}
 		while (xDummy != 0 && divisor != 0) {
 			if (xDummy < 10 && xDummy > -10)
