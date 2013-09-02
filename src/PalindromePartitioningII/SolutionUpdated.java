b64af8c8fe26395d934af461392b10bfb4ad4fd4
wwr1227 <50442046@qq.com>
2013-09-02 14:04:16 +0800

    O(n^2)

--- src/PalindromePartitioningII/SolutionUpdated.java
+++ src/PalindromePartitioningII/SolutionUpdated.java
@@ -1,7 +1,7 @@
-
+//O(n^2)
 public class Solution {
-	public int minCut(String s) {
-		int ans = 0;
+    	public int minCut(String s) {
+    		int ans = 0;
 		if (s == null || s.length() == 0)
 			return ans;
 		boolean[][] isPa = new boolean[s.length()][s.length()];
@@ -11,16 +11,18 @@ public class Solution {
 						&& (j - i < 2 || isPa[i + 1][j - 1]))
 					isPa[i][j] = true;
 		int[] paLen = new int[s.length()];
-		for (int i = 0; i < s.length(); i++) {
-			paLen[i] = s.length() - i - 1;
-		}
-		for (int i = s.length() - 1; i >= 0; i--) {
-			for (int j = i; j < s.length(); j++) {
-				if (isPa[i][j]) {
-					paLen[j] = Math.min(paLen[j], paLen[j + 1] + 1);
-				}
+		for (int i = 0; i < s.length(); i++)
+			paLen[i] = Integer.MAX_VALUE;
+		paLen[0]=0;
+		for (int i = 0; i < s.length(); i++){
+			for (int j = 0; j < i; j++){
+				if (isPa[0][i])
+					paLen[i] = 0;
+				if (isPa[j+1][i])
+					paLen[i] = Math.min(paLen[i], paLen[j] + 1);
 			}
 		}
-		return paLen[0];
+		return paLen[s.length()-1];
+
 	}
 }
