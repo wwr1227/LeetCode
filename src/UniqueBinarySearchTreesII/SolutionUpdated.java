3b11b5844cbbcab5c975eaa731487fcb47303a0f
wwr1227 <50442046@qq.com>
2013-09-03 16:51:56 +0800

    O(Catalan(n))

--- src/UniqueBinarySearchTreesII/SolutionUpdated.java
+++ src/UniqueBinarySearchTreesII/SolutionUpdated.java
@@ -1,3 +1,4 @@
+//O(Catalan(n))
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -8,29 +9,27 @@
  * }
  */
 public class Solution {
-    public ArrayList<TreeNode> generateTrees(int n) {
+    	public ArrayList<TreeNode> generateTrees(int n) {
         // Start typing your Java solution below
         // DO NOT write main() function
 		return generateTreesHandler(1, n);
-    }
-	public ArrayList<TreeNode> generateTreesHandler(int start, int end){
+    	}
+	public ArrayList<TreeNode> generateTreesHandler(int start, int end) {
 		ArrayList<TreeNode> trees = new ArrayList<TreeNode>();
-		if(start>end){
+		if (start > end) {
 			trees.add(null);
 			return trees;
 		}
-		for(int mid = start; mid<=end; mid++){
-			ArrayList<TreeNode>  left = generateTreesHandler(start, mid-1);
-			ArrayList<TreeNode>  right = generateTreesHandler(mid+1, end);
-			for(int i = 0;i<left.size();i++){
-				for(int j = 0;j<right.size();j++){
-					TreeNode root = new TreeNode(mid);
-					root.left = left.get(i);
-					root.right = right.get(j);
+		for (int i = start; i <= end; i++) {
+			for (TreeNode left : generateTreesHandler(start, i - 1)) {
+				for (TreeNode right : generateTreesHandler(i + 1, end)) {
+					TreeNode root = new TreeNode(i);
+					root.left = left;
+					root.right = right;
 					trees.add(root);
 				}
 			}
 		}
-		return trees;	
+		return trees;
 	}
 }
