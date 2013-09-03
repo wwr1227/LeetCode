82fe4852b8622fc276c3bb0a3bb111ede53ab05a
wwr1227 <50442046@qq.com>
2013-09-03 16:14:46 +0800

    O(n)

---
+++ src/ConvertSortedListtoBinarySearchTree/Solution_Recursion.java
@@ -0,0 +1,48 @@
+//O(n)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head==null)
+			return null;
+		int len=0;
+        ListNode cur = head;
+		while(cur!=null){
+			len++;
+			cur=cur.next;
+		}
+		return generateBST(head,0,len-1);
+    }
+	public TreeNode generateBST(ListNode head,int left,int right){
+		if(left>right)
+			return null;
+		int mid = left+(right-left)/2;
+		TreeNode leftTree = generateBST(head,left,mid-1);
+		TreeNode root = new TreeNode(head.val);
+		root.left=leftTree;
+		if (head.next != null) { // "move to next"
+			head.val = head.next.val;
+			head.next = head.next.next;
+		}
+		TreeNode rightTree=generateBST(head,mid+1,right);
+		root.right=rightTree;		
+		return root;
+	}
+}
