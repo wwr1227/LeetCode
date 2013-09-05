68044cebafd80d4cd1946b5d383f21d810b7ddaa
wwr1227 <50442046@qq.com>
2013-09-05 17:20:04 +0800

    //O(n)

--- src/CountandSay/SolutionUpdated.java
+++ src/CountandSay/SolutionUpdated.java
@@ -1,29 +1,28 @@
 //O(n)
 // 1, 11, 21, 1211, 111221, .
 public String countAndSay(int n) {
-		if (n < 1)
-			return "";
-
-		int i = 1;
-		StringBuffer pre = new StringBuffer("1");
-		StringBuffer cur = new StringBuffer();
-		while (i < n) {
-			cur.delete(0, cur.length());
-			for (int j = 0; j < pre.length(); j++) {
-				int count = 1;
-				char say = pre.charAt(j);
-				while (j < pre.length() - 1 && say == pre.charAt(j + 1)) {
-					j++;
-					count++;
-				}
-				cur.append(count);
-				cur.append(say);
-
+	if (n < 1)
+		return "";
+	int i = 1;
+	StringBuffer pre = new StringBuffer("1");
+	StringBuffer cur = new StringBuffer("1");
+	while (i < n) {
+		cur.delete(0, cur.length());
+		for (int j = 0; j < pre.length(); j++) {
+			int count = 1;
+			char say = pre.charAt(j);
+			while (j < pre.length() - 1 && say == pre.charAt(j + 1)) {
+				j++;
+				count++;
 			}
-			pre.delete(0, pre.length());
-			pre.append(new StringBuffer(cur));
-			i++;
+			cur.append(count);
+			cur.append(say);
 
 		}
-		return cur.toString();
+		pre.delete(0, pre.length());
+		pre.append(new StringBuffer(cur));
+		i++;
+
+	}
+	return cur.toString();
 }
