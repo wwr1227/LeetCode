a8eea94d9b46a4fe54d7070330b834dc6a8489ab
wwr1227 <50442046@qq.com>
2013-09-07 10:43:42 +0800

    O(n)

--- src/JumpGameII/SolutionUpdated.java
+++ src/JumpGameII/SolutionUpdated.java
@@ -1,23 +1,23 @@
-//Time complexity:O(n^2)
+//Time complexity:O(n)
 public class Solution {
     public int jump(int[] A) {
         // Start typing your Java solution below
         // DO NOT write main() function
         if(A==null || A.length<=1)
             	return 0;
-		int maxDistance = 0;
-		int minStep=0;
-		int end=0;
-		int begin=0;
-		while(end<A.length-1){
-		    minStep++;
-			for(int i=begin;i<=end;i++)
-				maxDistance = Math.max(maxDistance, A[i]+i);
-			if(maxDistance<=end)
-				return -1;
-			begin=end+1;
-			end=maxDistance;
-		}
-		return minStep;
+	int maxDistance = 0;
+	int minStep=0;
+	int end=0;
+	int begin=0;
+	while(end<A.length-1){
+	    minStep++;
+		for(int i=begin;i<=end;i++)
+			maxDistance = Math.max(maxDistance, A[i]+i);
+		if(maxDistance<=end)
+			return -1;
+		begin=end+1;
+		end=maxDistance;
+	}
+	return minStep;
     }
 }
