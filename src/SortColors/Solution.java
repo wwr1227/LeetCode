7a6edb2492f874b499dc9e5f19484379ce1a3331
wwr1227 <50442046@qq.com>
2013-09-13 08:52:45 +0800

    Update Solution.java

--- src/SortColors/Solution.java
+++ src/SortColors/Solution.java
@@ -34,3 +34,39 @@ public class Solution {
 		A[b] = temp;
 	}
 }
+
+//----------------------------------------------------
+public class Solution {
+    public void sortColors(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(A==null||A.length==0)
+            return;
+        int redIndex=0;
+        int blueIndex=A.length-1;
+        int i=0;
+        while(i<A.length&&redIndex<blueIndex){
+            if(A[i]==0){
+                if(redIndex<i){
+                    swap(A,i,redIndex++);
+                    continue;
+                }
+            }
+            if(A[i]==2){
+                if(blueIndex>i){
+                    swap(A,i,blueIndex--);
+                    continue;
+                }
+            }
+            i++;
+        }
+        
+    }
+    public void swap(int [] a, int i ,int j){
+        int temp =a[i];
+        a[i] = a[j];
+        a[j]=temp;
+        
+    }
+    
+}
