56d91c8a5934a82aaeb2645d5380241454a74037
wwr1227 <50442046@qq.com>
2013-09-14 11:35:48 +0800

    Update SolutionUpdated.java

--- src/Combinations/SolutionUpdated.java
+++ src/Combinations/SolutionUpdated.java
@@ -26,3 +26,36 @@ public class Solution {
         combineHandler(n,begin+1,step,k,item,nums);
     }
 }
+//=================================================================================================
+public class Solution {
+   public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+    	// Start typing your Java solution below
+		// DO NOT write main() function
+		ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+		if (k <= 0 || k > n)
+			return ans;
+		ArrayList<Integer> item = new ArrayList<Integer>();
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++)
+			nums[i] = i + 1;
+		dfs(nums, n, k, 0, ans, item);
+		return ans;
+	}
+
+	public void dfs(int[] nums, int n, int k, int start,
+			ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> item) {
+		if (item.size() == k) {
+			if (ans.contains(item))
+				return;
+			ans.add(new ArrayList<Integer>(item));
+			return;
+		}
+		if (start >= n)
+			return;
+		dfs(nums, n, k, start + 1, ans, item);
+		item.add(nums[start]);
+		dfs(nums, n, k, start + 1, ans, item);
+		item.remove(item.size() - 1);
+	}
+
+}
